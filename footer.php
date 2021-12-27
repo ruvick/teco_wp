816001da769b4e077a88afdc9a79d87737345bc3
Ruvick <rudikov.web@ya.ru>
2021-12-27 20:30:55 +0300

    Телефон из полей

--- footer.php
+++ footer.php
@@ -9,8 +9,8 @@
 			    'container_class' => 'menu-list footer__menu-list d-flex','container' => false )); ?>
 
 			<div class="contacts d-flex">
-				<a href="tel:88003454567" class="contacts__phone">8 800 345-45-67</a>
-				<!-- <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>"><? echo $tel = carbon_get_theme_option("as_phone_1"); ?></a> -->
+			<? $tel = carbon_get_theme_option("as_phones_1"); 
+					if (!empty($tel)){?><a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="contacts__phone"><? echo $tel; ?></a><?}?>
 				<p class="contacts__time-phone">Бесплатный звонок по России</p>
 			</div>
 
--- template-parts/header-index-section.php
+++ template-parts/header-index-section.php
@@ -10,12 +10,11 @@
 						'container_class' => 'menu-list header__menu-list','container' => false )); ?>
 
         <div class="contacts header__contacts d-flex">
-          <a href="tel:88003454567" class="contacts__phone header__phone">8 800 345-45-67</a>
-          <!-- <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>"><? echo $tel = carbon_get_theme_option("as_phone_1"); ?></a> -->
-          <p class="contacts__time-phone header__time-phone">Бесплатный звонок по России</p>
+        <? $tel = carbon_get_theme_option("as_phones_1"); 
+					if (!empty($tel)){?><a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="contacts__phone header__phone"><? echo $tel; ?></a><?}?>
+          <p class="contacts__time-phone header__time-phone">Бесплатный звонок по России</p> 
         </div>
-        <a href="tel:88003454567" class="mob-phone-icon header__mob-phone-icon"></a>
-        <!-- <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="mob-callback__phone"></a> -->
+        <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="mob-phone-icon header__mob-phone-icon"></a>
 
         <a href="#callback" class="header__popup-link _popup-link	btn btn_white">Заказать звонок</a>
 
