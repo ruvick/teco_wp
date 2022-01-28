873745bb89c14c56f8ceea0e7d8015a0032843c6
asmi046 <asmi046@gmail.com>
2022-01-28 17:17:04 +0300

    Тег мейла

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/img/favicons/icon32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/img/favicons/icon16.png" sizes="16x16">
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/img/favicons/fav.svg" sizes="any">
-
+  <meta name="mailru-domain" content="WFVTYJlbc9ijiOhZ" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 
   <?php wp_head();?>   
