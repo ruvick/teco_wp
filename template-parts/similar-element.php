e174ea75f106359bb0d8827adecd9f6b9a215a32
Ruvick <rudikov.web@ya.ru>
2022-01-30 21:25:20 +0300

    Кликабельная картинка

--- template-parts/similar-element.php
+++ template-parts/similar-element.php
@@ -1,11 +1,11 @@
 <div class="product__column similar-products__column">
   <div class="product__card">
-    <div class="product__card-img">
+    <a href="<?php echo get_permalink();?>" class="product__card-img"> 
       <picture>
         <img src="<?php $imgTm = get_the_post_thumbnail_url(get_the_ID(), "tominiatyre");
 					echo empty($imgTm) ? get_bloginfo("template_url") . "/img/no-photo.jpg" : $imgTm; ?>" alt="<? the_title(); ?>" loading="lazy">
 		  </picture>      
-    </div>
+    </a>
     <div class="product__card-descp">
       <h6 class="product__card-descp-title"><?php echo $post->post_title?></h6>
     </div>
