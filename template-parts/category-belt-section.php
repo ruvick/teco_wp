65c8b441ab0b0eb89c87bcf5ebcd3f19ce86e6e0
asmi046 <asmi046@gmail.com>
2022-03-21 18:04:44 +0300

    Вывод таблицы

--- template-parts/category-belt-section.php
+++ template-parts/category-belt-section.php
@@ -7,6 +7,18 @@
 		
 		<div class="beltCatWrapper beltCatWrapper_list">
 			<?php if ( have_posts() ) : ?> 
+				<div class = "hab_belt_blk hab_belt_blk_head">
+						<div class="image_h">Изображение</div>
+						<div class="title">Артикул:</div>
+						<div class="tolsh">Толщина:</div>
+						<div class="sila_t">Сила тяги (k1%):</div>
+						<div class="val_min">Вал (min):</div>
+						<div class="material">Материал:</div>
+						<div class="np">Нож. переход:</div>
+						<div class="count_sl">Колличество слоев:</div>
+						<div class="count_tyg_sl">Тяг. слой:</div>
+				</div>
+				
 				<?while ( have_posts() ) : the_post(); ?>
 					<div class = "hab_belt_blk hab_belt_blk_pl">
 						<div class="image">
@@ -16,15 +28,50 @@
 							 
 						</div>
 
-						<div class="title"><? the_title(); ?></div>
-						<div class="tolsh"><? echo carbon_get_post_meta(get_the_ID(), "belt_tolsh"); ?></div>
-						<div class="sila_t"><? echo carbon_get_post_meta(get_the_ID(), "belt_sila_t"); ?></div>
-						<div class="val_min"><? echo carbon_get_post_meta(get_the_ID(), "belt_val_min"); ?></div>
-						<div class="material"><? echo carbon_get_post_meta(get_the_ID(), "belt_material"); ?></div>
-						<div class="np"><? echo carbon_get_post_meta(get_the_ID(), "belt_np"); ?></div>
-						<div class="count_sl"><? echo carbon_get_post_meta(get_the_ID(), "belt_count_sl"); ?></div>
-						<div class="count_tyg_sl"><? echo carbon_get_post_meta(get_the_ID(), "belt_tyg_sl"); ?></div>
-						<div class="dop"><? echo carbon_get_post_meta(get_the_ID(), "belt_dop"); ?></div>
+						<div class="title"> 
+							<span class = "mobile_title">Артикул:</span> 
+							<? the_title(); ?>
+						</div>
+
+						<div class="tolsh">
+							<span class = "mobile_title">Толщина:</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_tolsh"); ?>
+						</div>
+
+						<div class="sila_t">
+							<span class = "mobile_title">Сила тяги (k1%):</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_sila_t"); ?>
+						</div>
+
+						<div class="val_min">
+							<span class = "mobile_title">Вал (min):</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_val_min"); ?>
+						</div>
+						
+						<div class="material">
+							<span class = "mobile_title">Материал:</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_material"); ?>
+						</div>
+
+						<div class="np">
+							<span class = "mobile_title">Нож. переход:</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_np"); ?>
+						</div>
+
+						<div class="count_sl">
+							<span class = "mobile_title">Колличество слоев:</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_count_sl"); ?>
+						</div>
+						
+						<div class="count_tyg_sl">
+							<span class = "mobile_title">Тяг. слой:</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_tyg_sl"); ?>
+						</div>
+
+						<div class="dop">
+							<span class = "mobile_title">Дополнительно:</span> 
+							<? echo carbon_get_post_meta(get_the_ID(), "belt_dop"); ?>
+						</div>
 					</div>	
 
 				<?php endwhile;?>
