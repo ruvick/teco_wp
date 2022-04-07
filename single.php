8444c4a39c93fe70553c246d4eef8129fad9deb6
Ruvick <rudikov.web@ya.ru>
2022-04-07 19:14:35 +0300

    Поправил иконку

--- single.php
+++ single.php
@@ -23,7 +23,7 @@ get_header(); ?>
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<h1 class="title-navigation__title"><?php the_title();?></h1>
 					<?php the_content();?>
-					<?php endwhile;?>
+					<?php endwhile;?> 
 				<?php endif; ?> 
 
 			</div>
