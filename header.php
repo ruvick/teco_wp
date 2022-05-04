0f7549391e33da2ab94ad64bb0bea7f383f39cfe
asmi046 <asmi046@gmail.com>
2022-05-04 18:07:58 +0300

    Метрика

--- header.php
+++ header.php
@@ -22,6 +22,22 @@
 </head>
 <body>
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(88698859, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/88698859" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php
 	$sload_id = 'c4c46acb96d37c6e0a14b5ec66f5d8ad';
 	$sload_opt = stream_context_create(array('https'=>array('timeout'=>2)));  
